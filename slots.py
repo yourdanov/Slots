ca8ecf7aa3ffb5c07331623132d9ffe39e65185c
Atanas Yourdanov <113124786+yourdanov@users.noreply.github.com>
2024-07-01 04:52:37 +0300

    Reforge of the source code.

--- slots.py
+++ slots.py
@@ -3,10 +3,12 @@ from tkinter import messagebox, Menu, colorchooser, simpledialog
 import random
 import os
 import matplotlib as mpl
-if os.environ.get('DISPLAY','') == '':
+
+if os.environ.get('DISPLAY', '') == '':
     print('no display found. Using non-interactive Agg backend')
     mpl.use('Agg')
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
 
 def show_about():
     messagebox.showinfo("About", "Slots Machine Game\nCreated by Atanas Yourdanov")
